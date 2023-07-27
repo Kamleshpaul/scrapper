110b4d6b935949d6015d678c5d69f21b5f1612d7
Kamlesh Paul <kamleshpaul414@gmail.com>
2023-07-27 20:04:25 +0530

    wip

--- igScrapper.js
+++ igScrapper.js
@@ -1,11 +1,14 @@
-const puppeteer = require('puppeteer');
-const { program } = require('commander');
+const puppeteer = require("puppeteer");
+const { program } = require("commander");
 
 program
-  .version('1.0.0')
-  .arguments('<url>')
+  .version("1.0.0")
+  .arguments("<url>")
   .action(async (url) => {
-    const username = await scrapeInstagramUsername(url, { headless: 'new' }); 
+    const username = await scrapeInstagramUsername(url, {
+      headless: "new",
+      args: ["--no-sandbox"],
+    });
     console.log(username);
   })
   .parse(process.argv);
@@ -17,29 +20,33 @@ async function scrapeInstagramUsername(url, options) {
 
     // Emulate a real user agent to avoid detection
     await page.setUserAgent(
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
     );
 
-    await page.goto(url, { waitUntil: 'networkidle2' });
+    await page.goto(url, { waitUntil: "networkidle2" });
 
     const isFollowButton = await page.evaluate(() => {
-      const buttons = Array.from(document.querySelectorAll('button'));
-      return buttons.some(button => button.textContent.includes('Follow'));
+      const buttons = Array.from(document.querySelectorAll("button"));
+      return buttons.some((button) => button.textContent.includes("Follow"));
     });
 
     if (!isFollowButton) {
-      console.log('Profile is private or follow button not found.');
+      console.log("Profile is private or follow button not found.");
       await browser.close();
       process.exit(0);
     }
 
     const username = await page.evaluate(() => {
-      const followButton = Array.from(document.querySelectorAll('button'))
-        .find(button => button.textContent.includes('Follow'));
+      const followButton = Array.from(document.querySelectorAll("button")).find(
+        (button) => button.textContent.includes("Follow")
+      );
 
       if (followButton) {
-        const aTag = followButton.parentElement.parentElement.querySelector('a');
-        return aTag ? aTag.getAttribute('href').replace('/', '').replace('/', '') : null;
+        const aTag =
+          followButton.parentElement.parentElement.querySelector("a");
+        return aTag
+          ? aTag.getAttribute("href").replace("/", "").replace("/", "")
+          : null;
       }
       return null;
     });
@@ -47,7 +54,7 @@ async function scrapeInstagramUsername(url, options) {
     await browser.close();
     return username;
   } catch (err) {
-    console.error('Error fetching data from Instagram:', err);
+    console.error("Error fetching data from Instagram:", err);
     process.exit(1);
   }
 }
